c3cc73bf3cf2ec677203a3312930eea19bd5921c
Dmitry Balahonov <balahonov.dmitry@gmail.com>
2019-10-19 11:17:13 +0300

    feat: add class Node, constr, appendChihld, remove

--- src/node.js
+++ src/node.js
@@ -1,14 +1,32 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
 	appendChild(node) {
-
+		if (!this.left || !this.right) {
+            if (!this.left) {
+                this.left = node;
+                node.parent = this;
+            } else {
+                this.right = node;
+                node.parent = this;
+            }
+        }
 	}
 
 	removeChild(node) {
-
+		if (node === this.left){
+            this.left = null;
+            node.parent = null;
+        } else if (node === this.right){
+            this.right = null;
+            node.parent = null;
+        } else throw Error;
 	}
 
 	remove() {
